d078a20acea1b218be7a4c33d952637f57ca381a
Mateusz Kozak <koziu92@gmail.com>
2016-06-07 16:00:15 +0200

    ResourceServer

---
+++ DarehService.ResourceServer/Controllers/ProtectedController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace DarehService.ResourceServer.Controllers
+{
+  [Authorize]
+  [RoutePrefix("api/protected")]
+  public class ProtectedController : ApiController
+  {
+    [Route("")]
+    public IEnumerable<object> Get()
+    {
+      var identity = User.Identity as ClaimsIdentity;
+
+      if (identity != null)
+        return identity.Claims.Select(c => new
+        {
+          Type = c.Type,
+          Value = c.Value
+        });
+      return null;
+    }
+  }
+}
---
+++ DarehService.ResourceServer/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using DarehService.ResourceServer.App_Start;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(DarehService.ResourceServer.Startup))]
+namespace DarehService.ResourceServer
+{
+  public class Startup
+  {
+    public void Configuration(IAppBuilder app)
+    {
+      var config = new HttpConfiguration();
+
+      ConfigurationOAuth(app);
+
+      WebApiConfig.Register(config);
+
+    }
+
+    private void ConfigurationOAuth(IAppBuilder app)
+    {
+      app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions
+      {
+
+      });
+    }
+  }
+}
